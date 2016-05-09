8ac997c841dd92733a79b0179e7a49b3f3482d17
asidor <adrian.sidor@icompass.pl>
2016-05-09 12:37:27 +0200

    added cplex jar to classpath

--- generators/src/main/java/generators/Main.java
+++ generators/src/main/java/generators/Main.java
@@ -1,9 +1,16 @@
 package generators;
 
+import ilog.concert.*; 
+import ilog.cplex.*;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	       try {
+	    	   IloCplex cplex = new IloCplex();
+	       } catch (IloException e) {
+	    	   System.err.println("Concert exception caught: " + e); 
+	       }
 
 	}
 
